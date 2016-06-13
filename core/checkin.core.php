3df8d20a4ca3abb0adbccd0c9444a9a8fdbffb93
AnClark <clarklaw4701@qq.com>
2016-06-13 12:06:22 +0000

    继续完善

--- core/checkin.core.php
+++ core/checkin.core.php
@@ -6,8 +6,7 @@
 	
 	mysql_select_db(DB_HR, $con);
 	
-	$sql = "INSERT INTO " . TBL_HR . "(
-		ID,
+	$sql = "INSERT INTO " . TBL_HR . " (
 		Name,
 		Gender,
 		Birthday,
@@ -20,31 +19,47 @@
 		GroupInDepart,
 		Occupation,
 		AUID,
-		ArrivalTime,
-		Mobile,
-		QQ
+		ArrivalTime
 		)
 		VALUES (
-		$_POST['ID'],
-		$_POST['Name'].
-		$_POST['Gender'],
-		$_POST['Birthday'],
-		$_POST['Faculty'],
-		$_POST['Class'],
-		$_POST['Grade'],
-		$_POST['Dormbuild'],
-		$_POST['Hometown'],
-		$_POST['Department'],
-		$_POST['GroupInDepart'],
-		$_POST['Occupation'],
-		$_POST['AUID'],
-		$_POST['ArrivalTime'],
-		$_POST['Mobile'],
-		$_POST['QQ']
+		'$_POST[Name]',
+		'$_POST[Gender]',
+		'$_POST[Birthday]',
+		'$_POST[Faculty]',
+		'$_POST[Class]',
+		'$_POST[Grade]',
+		'$_POST[Dormbuild]',
+		'$_POST[Hometown]',
+		'$_POST[Department]',
+		'$_POST[GroupInDepart]',
+		'$_POST[Occupation]',
+		'$_POST[AUID]',
+		'$_POST[ArrivalTime]'
 		)";
 	
-	if(mysql_query($sql)){
-		
+	$sql = test_input($sql);
+	
+	try{
+		if(mysql_query($sql)){
+			echo "<script> alert('已录入数据'); </script>";
+		}
+		else{
+			echo mysql_error();
+			//echo "<script> alert('操作失败：$errmsg'); </script>";
+			echo "<script> alert('操作失败'); </script>";
+		}
+	}
+	catch(Exception $e){
+		$errmsg = $e->getMessage;
+		echo "<script> alert('出现错误：$errmsg'); </script>";
 	}
 	
+	
+/*********   检查函数，用于检查用户输入并摒弃无效字符  ************/	
+	function test_input($data){
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
 ?>

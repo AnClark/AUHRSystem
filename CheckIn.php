0ecc4faaf6cd845896c80a768dabcca96a0b4e39
AnClark <clarklaw4701@qq.com>
2016-06-21 03:03:42 +0000

    用回之前的登录验证方式。反正页面代码都有重复

--- CheckIn.php
+++ CheckIn.php
@@ -45,9 +45,31 @@
 
 <!--		【内嵌 PHP 代码，实现奇妙功能！】		-->
 <?php
-/***********	检验登录状态	***********/
-//为简洁起见，相关代码放入独立文件中
-include('core/logIdentify.php');
+/*********************	登录状态检测程序	*********************/
+
+session_start();
+
+//检测是否登录，若没登录则转向登录界面
+if(!isset($_SESSION['userid'])){
+	echo "<script> alert('你还未登录，请先登录后再使用！'); </script>";
+	echo "<script> location.href = 'login.html'; </script>";
+	
+    //header("Location: login.html");
+    exit();
+}
+
+$userid = $_SESSION['userid'];
+$username = $_SESSION['username'];
+$user_indicator_text = "你好,  " . $_SESSION['username'];
+
+switch($_SESSION['userlevel']){
+	case "user":
+		$user_level_text = "普通用户";
+		break;
+	case "admin":
+		$user_level_text = "高级用户";
+		break;
+}
 ?>
 
 <!-----------------------页首部分----------------------->
